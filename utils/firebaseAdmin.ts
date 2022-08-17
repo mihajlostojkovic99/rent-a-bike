d0e320d4a009915d65530b81110b9a81c59a00a7
Mihajlo Stojkovic <70921912+mihajlostojkovic99@users.noreply.github.com>
2022-08-17 11:42:25 +0200

    fix for the fix

--- utils/firebaseAdmin.ts
+++ utils/firebaseAdmin.ts
@@ -1,12 +1,13 @@
 import admin from 'firebase-admin';
-const serviceAccount = require('../secret.json');
+// const serviceAccount = require('../secret.json');
+const serviceAccount = JSON.parse(
+  process.env.GOOGLE_APPLICATION_CREDENTIALS as string,
+);
 
 export const verifyIdToken = (token: string) => {
   if (!admin.apps.length) {
     admin.initializeApp({
-      credential: admin.credential.cert(
-        process.env.GOOGLE_APPLICATION_CREDENTIALS ?? serviceAccount,
-      ),
+      credential: admin.credential.cert(serviceAccount),
       databaseURL: 'https://diplomski-2022.firebaseio.com',
     });
   }
